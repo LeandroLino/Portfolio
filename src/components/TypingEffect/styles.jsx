afcca8fd9cf76a6a81aeea2eada0cc743bf7580a
LeandroLino <leao.lino7@gmail.com>
2023-08-07 17:25:05 -0300

    FEATURE :: HOME :: MOBILE ADDED

--- src/components/TypingEffect/styles.jsx
+++ src/components/TypingEffect/styles.jsx
@@ -11,6 +11,12 @@ export const Cursor = styled.span`
     color: #fff;
     font-weight: 100;
     font-size: 110px;
+    @media (max-width: 768px) {
+        font-size: 80px;
+    }
+    @media (max-width: 500px) {
+        font-size: 60px;
+    }
     display: flex;
   
   @keyframes blink-caret {
--- src/pages/Home/styles.jsx
+++ src/pages/Home/styles.jsx
@@ -18,6 +18,15 @@ export const Container = styled.body`
 export const Title = styled.span`
   color: #555;
   font-size: 1.5rem;
+  @media (max-width: 768px) {
+        font-size: 1.0rem;
+    }
+    @media (max-width: 500px) {
+        width: 60%;
+    }
+    @media (max-width: 400px) {
+        width: 80%;
+    }
   font-weight: bold;
 
   width: 50%;
@@ -33,6 +42,16 @@ export const Title = styled.span`
 
 export const DevTitle = styled.span`
     color: #fff;
+    @media (max-width: 768px) {
+        font-size: 90px;
+        height: 10%;
+    }
+    @media (max-width: 500px) {
+        font-size: 60px;
+    }
+    @media (max-width: 450px) {
+        font-size: 50px;
+    }
     font-size: 120px;
 
     font-weight: bold;
@@ -44,5 +63,14 @@ export const DevTitle = styled.span`
 
 export const TechsTitle = styled(DevTitle)`
   width: 50%;
+  @media (max-width: 768px) {
+    width: 65%;
+    }
+    @media (max-width: 500px) {
+        width: 70%;
+    }
+    @media (max-width: 380px) {
+        width: 80%;
+    }
   color: #FCBA03;
 `;
